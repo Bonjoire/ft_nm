0db10702b76a279a9141b2e1acd0a6b5354486e2
hubourge <hulo1920@gmail.com>
2025-02-19 19:14:04 +0100

    Fix ./ft_nm with no arg

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hubourge <hubourge@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: hubourge <hubourge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:34:34 by hubourge          #+#    #+#             */
-/*   Updated: 2025/02/10 17:37:20 by hubourge         ###   ########.fr       */
+/*   Updated: 2025/02/19 19:12:37 by hubourge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,18 @@ void	nm(int ac, char **av)
 		if ((av[i] && av[i][0] != '-') || data.nb_files == 0)
 		{
 			init_data(&data, DO_NOT_INIT_OPT);
-			if (!file_rights(&data, av[i]))
+			data.file = av[i];
+			if (data.nb_files == 0)
+				data.file = "a.out";
+			data.nb_files++;
+
+			if (!file_rights(&data, data.file))
 			{
 				i++;
 				continue;
 			}
 			if (data.nb_files > 1)
 				ft_printf(STDOUT_FILENO, "\n%s:\n", av[i]);
-
-			data.file = av[i];
-			if (data.nb_files == 0)
-				data.file = "a.out";
-			data.nb_files++;
 			
 			map_file(&data);
 			detect_valid_elf(&data, data.mapped_file);
